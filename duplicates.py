c4d72ebe0b02556c03e51fa7b32534390811d135
Sergey Piterskikh <piterskikhsa@mail.ru>
2017-08-08 21:36:46 +0300

    Fixed output of results. Fixed some small bugs

--- duplicates.py
+++ duplicates.py
@@ -3,9 +3,9 @@ import sys
 import os
 from collections import defaultdict
 
-defdict = defaultdict(dict)
 
 def get_files(path):
+    defdict = defaultdict(dict)
     for root, dirs, files in os.walk(path):
         for file in files:
             file_path = os.path.join(root, file)
@@ -16,18 +16,37 @@ def get_files(path):
                 defdict[file][file_size] = [file_path]
     return defdict
 
+
 def get_duplicates(files_dict):
-    duplicates_files = []
+    duplicates = {}
     for file_name, file_data in files_dict.items():
         for file_size, file_path in file_data.items():
-            if len(file_path)> 1:
-                print(file_name, file_path, file_size)
+            if len(file_path) >= 2:
+                duplicates[file_name] = file_data
+    return duplicates
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         file_path = sys.argv[1]
     else:
-        file_path = input('Укажите путь к коталогу: ')
-    all_files = get_files(file_path)
-    get_duplicates(all_files)
+        file_path = input('Укажите путь к каталогу: ')
+    if os.path.exists(file_path):
+        file_path = os.path.abspath(file_path)
+        all_files = get_files(file_path)
+        all_duplicates = get_duplicates(all_files)
+        if all_duplicates:
+            for duplicates_name, duplicates_data in all_duplicates.items():
+                for file_size, file_path in duplicates_data.items():
+                    print("""
+Имя найденных дубликатов: {name}
+Размер: {size} байт
+Количество найденных дубликатов: {quantity}
+Пути к файлам: {file_path}""".format(name=duplicates_name, size=file_size,
+                                                     quantity=len(duplicates_data[file_size]),
+                                                     file_path=file_path))
+        else:
+            print('Ура! Дублитов не обнаруженно')
+    else:
+        print(
+            'Внимание: - Указанной папки не существует, проверьте правильность введенных данных, и попробуйте еще раз.')
